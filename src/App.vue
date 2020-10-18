d94fd070c4f3a9841af0cb68bd8048395ec5a0b7
YiroYi <yiroyi@live.com>
2020-10-18 00:06:03 -0500

    Add components

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    <h2>Hello my friends</h2>
+    <ul>
+      <friend-contact></friend-contact>
+      <friend-contact></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: 'Kripto',
+          name: "Kripto Yi",
+          phone: '434322',
+          mail: 'kripto@test.com'
+        },
+        {
+          id: "Baily",
+          name: "Baily Cobra",
+          phone: '434322',
+          mail: 'baily@test.com'
+        }
+      ]
+    }
+  }
+}
+
+</script>
---
+++ src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone: {{ friend.phone }} </strong></li>
+      <li><strong>Email: {{ friend.mail }}</strong></li>
+    </ul>
+  </li>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        detailsAreVisible: false,
+        friend: {
+          id: 'Kripto',
+          name: "Kripto Yi",
+          phone: '434322',
+          mail: 'kripto@test.com'
+        }
+      }
+    },
+    methods: {
+      toggleDetails() {
+        this.detailsAreVisible = !this.detailsAreVisible
+      }
+    }
+  };
+</script>
